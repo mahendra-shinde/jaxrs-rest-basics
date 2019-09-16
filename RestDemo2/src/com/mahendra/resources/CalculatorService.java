bc1c29cd5c07735d1899777362911e57d37f5bc3
Mahendra Chandrakant Shinde <mahendrashinde@synergetics-india.com>
2019-09-16 12:35:58 +0530

    Rest Server and client
    REST (JaxRS 2) on Glassfish

---
+++ RestDemo2/src/com/mahendra/resources/CalculatorService.java
@@ -0,0 +1,18 @@
+package com.mahendra.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+@Path("calc")
+@Produces("text/plain")
+public class CalculatorService {
+	@GET
+	@Path("add")
+	public String add(@QueryParam("n1") int n1, @QueryParam("n2") int n2)
+	{
+		return "Answer : "+ (n1+n2);
+	}
+
+}
